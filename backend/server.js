38c6b1af89edbd2730521ccf811f8c9dc6ccc0da
balakumar-arjunan <devbala1991.psg@gmail.com>
2025-03-14 16:55:24 +0530

    Connect to the database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log(`Server is running on port 5000!`);
-});
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT} and DB connected`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
